924f2f1773ac742bead5b086592968f0a83515a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 03:04:15 +0000

    Deploy zero-to-mastery/starwars-spaceships to github.com/zero-to-mastery/starwars-spaceships.git:gh-pages

---
+++ precache-manifest.1fb2bf88df87a6fdab84c1de8ec1d39f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb48f98aeef4e0002eff",
+    "url": "./static/css/main.d959e054.chunk.css"
+  },
+  {
+    "revision": "eb48f98aeef4e0002eff",
+    "url": "./static/js/main.eb48f98a.chunk.js"
+  },
+  {
+    "revision": "edf7bfbdcf917a8230a7",
+    "url": "./static/css/1.72058d3d.chunk.css"
+  },
+  {
+    "revision": "edf7bfbdcf917a8230a7",
+    "url": "./static/js/1.edf7bfbd.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c77e09ae4e2d1f9d925bebdfe1f239c0",
+    "url": "./static/media/spaceship.c77e09ae.png"
+  },
+  {
+    "revision": "31a02a31f03893ec77f479eed61f49c5",
+    "url": "./index.html"
+  }
+];
